819028110a59246b8d26bd990e5b88ddf2f7a1f7
unilock <unilock@fennet.rentals>
2025-02-12 18:59:34 -0500

    fix give command (this should go in Stacc...)
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/io/github/Andrew6rant/stacker/mixin/GiveCommandMixin.java
@@ -0,0 +1,14 @@
+package io.github.Andrew6rant.stacker.mixin;
+
+import net.minecraft.server.command.GiveCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(GiveCommand.class)
+public class GiveCommandMixin {
+	@ModifyVariable(method = "execute", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/argument/ItemStackArgument;createStack(IZ)Lnet/minecraft/item/ItemStack;"), index = 5)
+	private static int j(int value) {
+		return Integer.MIN_VALUE;
+	}
+}
